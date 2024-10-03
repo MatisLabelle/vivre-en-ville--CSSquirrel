857e69291af9d88a6d9201905a9a708de6906e48
ikramert <112107968+ikramert@users.noreply.github.com>
2024-10-03 12:35:18 -0400

    changes

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-// Alerte travail
-window.onload = function() {
-    if (!localStorage.getItem('alertDisplayed')) {
-        alert("Ce site a été créé dans le cadre de notre travail en projet web.");
-        localStorage.setItem('alertDisplayed', 'true');
-    }
-};
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
