8cfe857ceca7de52129d672f9d99ebc98de57202
MatisLabelle <mat48337@gmail.com>
2024-10-03 14:57:55 -0400

    anim-gsap acceuil

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".info-lettre",
+      start: "top bottom",
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  })
+  .from(".info-lettre__newsletter", { y: "-60vh", duration: 2 })
+  .from(".info-lettre__send", { y: "-60vh", x: "-100vw", duration: 2 });
+
+ScrollTrigger.refresh();
