7740fd23350726247211dfbdac4ab230f5342f73
jejed8 <47800238+jejed8@users.noreply.github.com>
2024-11-18 13:26:36 -0500

    Navbar finale

--- main.js
+++ main.js
@@ -41,3 +41,14 @@ for (let i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// NAVBAR
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
