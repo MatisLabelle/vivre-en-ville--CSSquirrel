f0ac1a5e326ad1d90151d5ea312116296b9ba67b
MatisLabelle <mat48337@gmail.com>
2024-11-25 13:22:33 -0500

    les pages statique sur wp

--- wp-content/themes/theme-de-base/header.php
+++ wp-content/themes/theme-de-base/header.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
     <?php wp_head(); 
 /* Cette fonction permet à WordPress et aux extensions d'instancier des fichier CSS et js dans le <head>
@@ -37,84 +38,81 @@
 	?>>
 
     <header>
-        <header>
-            <!--La NAV BAR-->
-            <!-- Top Bar Section -->
-            <div class="">
-                <div class="topbar">
-                    <div class="topbar-items">
-                        <a href="#" class="topbar-link">English</a>
-                        <a href="#" class="topbar-link">Recherche</a>
-                        <a href="#" class="topbar-link">S'abonner</a>
-                    </div>
+        <!--La NAV BAR-->
+        <!-- Top Bar Section -->
+        <div class="">
+            <div class="topbar">
+                <div class="topbar-items">
+                    <a href="#" class="topbar-link">English</a>
+                    <a href="#" class="topbar-link">Recherche</a>
+                    <a href="#" class="topbar-link">S'abonner</a>
                 </div>
             </div>
-            <nav>
-                <a class="logo" href="#">
-                    <img src="<?php echo get_template_directory_uri(); ?>/photos/LOGO.png" alt="logo" />
-                </a>
-                <ul>
-                    <li>
-                        <a href="/about.html">À propos</a>
-                    </li>
-                    <li>
-                        <a href="/news-hub.html">Actualités</a>
-                    </li>
-                    <li>
-                        <a href="/services-hub.html">Nos services</a>
-                    </li>
-                    <li>
-                        <a href="/equipe.html">Équipe</a>
-                    </li>
-                    <li>
-                        <a href="/contact.html">Contactez-nous</a>
-                    </li>
-                    <li>
-                        <button class="btn-donner">Donner</button>
-                    </li>
-                </ul>
-                <div class="nav-end">
-                    <button class="btn-donner-2">Donner</button>
-                    <div class="hamburger">
-                        <span class="line"></span>
-                        <span class="line"></span>
-                        <span class="line"></span>
-                    </div>
+        </div>
+        <nav>
+            <a class="logo" href="#">
+                <img src="<?php echo get_template_directory_uri(); ?>/photos/LOGO.png" alt="logo" />
+            </a>
+            <ul>
+                <li>
+                    <a href="/about.html">À propos</a>
+                </li>
+                <li>
+                    <a href="/news-hub.html">Actualités</a>
+                </li>
+                <li>
+                    <a href="/services-hub.html">Nos services</a>
+                </li>
+                <li>
+                    <a href="/equipe.html">Équipe</a>
+                </li>
+                <li>
+                    <a href="/contact.html">Contactez-nous</a>
+                </li>
+                <li>
+                    <button class="btn-donner">Donner</button>
+                </li>
+            </ul>
+            <div class="nav-end">
+                <button class="btn-donner-2">Donner</button>
+                <div class="hamburger">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
                 </div>
-            </nav>
-            <div class="menubar">
-                <ul>
-                    <li>
-                        <a href="/home.php">Accueil</a>
-                    </li>
-                    <li>
-                        <a href="/about.html">À propos</a>
-                    </li>
-                    <li>
-                        <a href="/news-hub.html">Actualités</a>
-                    </li>
-                    <li>
-                        <a href="/services-hub.html">Nos services</a>
-                    </li>
-                    <li>
-                        <a href="/equipe.html">Équipe</a>
-                    </li>
-                    <li>
-                        <a href="/contact.html">Contactez-nous</a>
-                    </li>
-                    <li>
-                        <a href="#">S'abonner</a>
-                    </li>
-                    <li>
-                        <a href="#">Rechercher</a>
-                    </li>
-                    <li>
-                        <a href="#">English</a>
-                    </li>
-                </ul>
             </div>
-
-        </header>
+        </nav>
+        <div class="menubar">
+            <ul>
+                <li>
+                    <a href="/home.php">Accueil</a>
+                </li>
+                <li>
+                    <a href="/about.html">À propos</a>
+                </li>
+                <li>
+                    <a href="/news-hub.html">Actualités</a>
+                </li>
+                <li>
+                    <a href="/services-hub.html">Nos services</a>
+                </li>
+                <li>
+                    <a href="/equipe.html">Équipe</a>
+                </li>
+                <li>
+                    <a href="/contact.html">Contactez-nous</a>
+                </li>
+                <li>
+                    <a href="#">S'abonner</a>
+                </li>
+                <li>
+                    <a href="#">Rechercher</a>
+                </li>
+                <li>
+                    <a href="#">English</a>
+                </li>
+            </ul>
+        </div>
     </header>
 
     <main>
--- wp-content/themes/theme-de-base/service.php
+++ wp-content/themes/theme-de-base/service.php
@@ -12,13 +12,69 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 ?>
 
 <article>
-    <?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-    <h2>
-        <?php the_title(); // Titre de la page ?>
-    </h2>
-    <?php endif; ?>
-
-    <?php the_content(); // Contenu principal de la page ?>
+    <section class="recherche">
+        <div class="breadcrumb"><a href="index.html">Accueil&nbsp;</a>
+            <p>>&nbsp;</p><a href="services-hub.html">Nos services&nbsp;</a>
+            <p>>&nbsp;</p><a href="service.html">Recherche et innovation</a>
+        </div>
+        <h1 class="recherche__title">Recherche et innovation</h1>
+        <h2 class="recherche__subTitle">Développer les connaissances</h2>
+        <p class="recherche__text">Pour diffuser auprès des intervenants québécois les meilleurs exemples de
+            développement
+            de collectivités viables, Vivre en Ville mène depuis 20 ans des recherches approfondies dans ce domaine. Une
+            veille scientifique permanente, des missions de recherche au Québec et à l’international, la rencontre
+            d’experts
+            de domaines variés, la participation à de nombreux colloques et conférences ont permis à son équipe de
+            développer une vision intégrée et des connaissances pointues sur les différents aspects du développement
+            viable
+            des collectivités.</p>
+        <div class="recherche__section">
+            <div class="bloc-1">
+                <h3 class="recherche__miniTitle">Recherches en cours</h3>
+                <p class="recherche__text">De nombreux thèmes sont abordés dans le cadre de notre travail en raison de
+                    leur
+                    pertinence et de leur actualité dans le contexte québécois. Voici quelques-uns des domaines de
+                    recherche qui
+                    occupent l'équipe en ce moment :</p>
+                <ul class="recherche__list">
+                    <li>le contrôle de l’étalement urbain,</li>
+                    <li>l’aménagement de milieux de vie de qualité,</li>
+                    <li>le déploiement d’une offre de mobilité durable,</li>
+                    <li>le climat et le mode de développement urbain,</li>
+                    <li>le lien entre environnement bâti et santé,</li>
+                    <li>les petites et moyennes collectivités,</li>
+                    <li>la localisation des activités,</li>
+                    <li>les questions sociales et la participation citoyenne, etc.</li>
+                </ul>
+            </div>
+            <div class="bloc-2">
+                <h3 class="recherche__miniTitle">Missions de recherche</h3>
+                <p class="recherche__text">L'équipe de Vivre en Ville effectue fréquemment des missions de recherche.
+                    Ces dernières sont destinées à étudier des exemples et pratiques ailleurs dans le monde afin
+                    d'alimenter la réflexion et de pouvoir transmettre ces connaissances aux acteurs québécois.</p>
+            </div>
+            <div class="bloc-3">
+                <img src="photos/image_service.jpg" alt="image_service">
+            </div>
+        </div>
+        <p class="recherche__text">Consultez les <a href="#" class="textLink">réalisations</a> de Vivre en Ville ou les
+            <a href="#" class="textLink">interventions par thématiques</a>.</p>
+        <h2 class="recherche__subTitle">Innover sur le terrain</h2>
+        <p class="recherche__text">À la fine pointe de la connaissance, Vivre en Ville met ses recherches à profit dans
+            le cadre de projets pilotes afin de stimuler l’émergence de nombreux projets innovants. Inspirées des
+            meilleures pratiques, nos idées prennent vie en partenariat avec des acteurs motivés.</p>
+        <h3 class="recherche__miniTitle">Nos projets pilotes</h3>
+        <ul>
+            <li>Végétalisation du <a href="#" class="textLink">Centre culture et environnement Frédéric Back &#8599;</a>
+            </li>
+            <li><a href="#" class="textLink">Verdir un milieu de vie</a> : Cœur vaillant</li>
+            <li>Mise en place des centres de gestion des déplacements de la Capitale Nationale et de l’Outaouais (<a
+                    href="#" class="textLink">Mobili-T &#8599;</a> et <a href="#" class="textLink">MOBI-O &#8599;</a>)
+            </li>
+            <li><a href="#" class="textLink">Aménager des milieux de vie favorables à un vieillissement en santé</a>
+            </li>
+        </ul>
+    </section>
 </article>
 <?php endwhile; // Fermeture de la boucle
 
@@ -26,6 +82,5 @@ else : // Si aucune page n'a été trouvée
 	get_template_part( 'partials/404' ); // Affiche partials/404.php
 endif;
 
-get_sidebar(); // Affiche le contenu de sidebar.php
 get_footer(); // Affiche footer.php 
 ?>
