784752821c6faccdfc946cd13688a14520d0de4e
MatisLabelle <mat48337@gmail.com>
2024-12-05 17:43:58 -0500

    htaccess

--- wp-content/themes/theme-de-base/header.php
+++ wp-content/themes/theme-de-base/header.php
@@ -1,8 +1,8 @@
 <?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Affiche l'entête (Header) sur toutes vos pages
-	/* Template Name: Header
-	/*-----------------------------------------------------------------------------------*/
+    /*-----------------------------------------------------------------------------------*/
+    /* Affiche l'entête (Header) sur toutes vos pages
+    /* Template Name: Header
+    /*-----------------------------------------------------------------------------------*/
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -14,8 +14,8 @@
         <?php bloginfo('name'); // Affiche le nom du blog ?> |
         <?php is_front_page() ? bloginfo('description') : wp_title(''); // si nous sommes sur la page d'accueil, affichez la description à partir des paramètres du site - sinon, affichez le titre du post ou de la page. ?>
     </title>
-    <?php 
-	// Tous les .css et .js sont chargés dans le fichier functions.php
+    <?php
+    // Tous les .css et .js sont chargés dans le fichier functions.php
 ?>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
@@ -23,32 +23,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
-    <?php wp_head(); 
+    <?php wp_head();
 /* Cette fonction permet à WordPress et aux extensions d'instancier des fichier CSS et js dans le <head>
-	 Supprimer cette fonction briserait vos extensions et diverses fonctionnalités WordPress. 
-	 Vous pouvez la déplacer si désiré, mais garder là. */
+     Supprimer cette fonction briserait vos extensions et diverses fonctionnalités WordPress.
+     Vous pouvez la déplacer si désiré, mais garder là. */
 ?>
 </head>
 
-<body <?php body_class(); 
-	/* Applique une classe contextuel sur le body
-		 ex: sur la page d'accueil vous aurez la classe "home"
-		 sur un article, "single postid-{ID}"
-		 etc. */
-	?>>
-    <div class="alert">
-        <div class="alert-content">
-            <p>Ce site web a été réalisé dans le cadre d'un projet scolaire et ne représente pas une plateforme
-                officielle.</p>
-            <button class="closebtn" onclick="this.parentElement.parentElement.style.display='none';">J'ai
-                compris</button>
-        </div>
-    </div>
+<body <?php body_class();
+    /* Applique une classe contextuel sur le body
+         ex: sur la page d'accueil vous aurez la classe "home"
+         sur un article, "single postid-{ID}"
+         etc. */
+    ?>>
+
     <header>
-
-
-
-
         <!--La NAV BAR-->
         <!-- Top Bar Section -->
         <div class="">
@@ -64,7 +53,7 @@
             <a class="logo" href="<?php echo home_url('/'); ?>">
                 <img src="<?php echo get_template_directory_uri(); ?>/photos/LOGO.png" alt="logo" />
             </a>
-            <?php 
+            <?php
             wp_nav_menu(array('theme_location' => 'main-menu'));
             ?>
             <button class="btn-donner">Donner</button>
@@ -78,11 +67,10 @@
             </div>
         </nav>
         <div class="menubar">
-            <?php 
+            <?php
   wp_nav_menu(array('theme_location' => 'main-menu'));
     ?>
         </div>
     </header>
 
     <main>
-        <!-- Débute le contenu principal de notre site -->
