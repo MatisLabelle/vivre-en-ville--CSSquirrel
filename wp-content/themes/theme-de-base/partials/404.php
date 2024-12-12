0c419b141b83332bedd75932d2e39215a20b147e
ikramert <112107968+ikramert@users.noreply.github.com>
2024-12-12 01:02:33 -0500

    404 complete

--- wp-content/themes/theme-de-base/partials/404.php
+++ wp-content/themes/theme-de-base/partials/404.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * Template Post Type: url, url-404
+ */
+
+get_header(); // Affiche header.php
+?>
+
 <section>
     <div class="404 erreur">
         <div class="mainbox">
@@ -6,8 +14,23 @@
                 <i class="far fa-question-circle fa-spin"></i>
                 <span class="err2">4</span>
                 <div class="msg">
-                    <a class="taville" href="./index.html">Cliques ici et retournons à ta ville. </a></p>
+                    <?php 
+                    // Récupérer le champ ACF 'url-404'
+                    $url_404 = get_field('url-404');
+                    
+                    // Utiliser l'URL si elle existe, sinon fallback vers une URL par défaut
+                    $fallback_url = 'http://localhost/css_squirrel_vivre_en_ville/index.php/acceuil/';
+                    $redirect_url = $url_404 ? $url_404 : $fallback_url;
+                    ?>
+                    <a class="taville" href="<?php echo esc_url($redirect_url); ?>">
+                        Cliques ici et retournons à ta ville.
+                    </a>
                 </div>
             </div>
         </div>
+    </div>
 </section>
+
+<?php
+get_footer(); // Affiche footer.php
+?>
